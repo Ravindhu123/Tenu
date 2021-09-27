3c642424b2aefc72ecb9d94bec1abbb60c9a7c6e
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-27 23:48:50 +0530

    Update Scrapers.js

--- plugins/Scrapers.js
+++ plugins/Scrapers.js
@@ -538,7 +538,7 @@ if (config.WORKTYPE == 'private') {
         await reply.delete();
     }));
 
-    Asena.addCommand({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
+    Asena.addCommand({pattern: 'image ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
         gis(match[1], async (error, result) => {
